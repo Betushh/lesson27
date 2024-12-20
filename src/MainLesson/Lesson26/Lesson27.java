99fa4f6cb01f1f742a3f6d0005067fc977303c23
Betushh <huseynzade.benovse@Gmail.com>
2024-12-21 03:07:24 +0400

    jeff comitted his first changes

---
+++ src/MainLesson/Lesson26/Lesson27.java
@@ -0,0 +1,18 @@
+package MainLesson.Lesson26;
+
+import java.util.Scanner;
+
+public class Lesson27 {
+    public static void main(String[] args) {
+
+        try{
+            Scanner scanner = new Scanner(System.in);
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+        } catch (ArithmeticException a) {
+            a.printStackTrace();
+        }finally {
+            System.out.println("it is jeff's doing");
+        }
+    }
+}
